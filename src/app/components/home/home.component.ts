7ced9db9321004f94d970c14326fd142b276870b
Alex Warnes <alex.s.warnes@gmail.com>
2020-02-20 23:48:20 -0500

    make header component reusable

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  title: string = 'TK360';
+  description: string = 'Complete design and remodel construction services.'
+  imgURL: string = "https://images.unsplash.com/photo-1481026469463-66327c86e544?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=824&q=80"
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
-
+  title: string = 'Full-Service Contractor'
+  description: string = 'No project is too small or big, and we even offer emergency service. We are licensed and insured and offer a wide range of services.'
+  imgURL: string = "https://images.unsplash.com/flagged/photo-1556438757-f9181aa300ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"
   constructor() { }
 
   ngOnInit(): void {
